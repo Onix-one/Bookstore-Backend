76b4a9bdf56c9e4363ec58a564bfcf6714eade46
Александр <U4iha312@mail.ru>
2022-01-12 00:14:25 +0300

    Add project files.

---
+++ Bookstore.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Bookstore.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ Bookstore.DAL.ADO/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Bookstore.DAL.ADO
+{
+    public class Class1
+    {
+    }
+}
